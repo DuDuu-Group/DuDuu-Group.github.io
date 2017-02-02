92699a5a667430491697692c967d2f9937d280fa
vivek malik <malik_vivek@network.lilly.com>
2017-02-02 14:26:08 +0530

    adding pages and routing

--- js/routes.js
+++ js/routes.js
@@ -1,26 +1,32 @@
 var app = angular.module('duApp', ['ngRoute']);
 
 app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
-    
-    
+    $locationProvider.hashPrefix('');
+
+    $routeProvider.when("/contact-us", {
+        templateUrl: "templates/contact-us.html"
+    });
+    $routeProvider.when("/blog", {
+        templateUrl: "templates/blog.html"
+    });
     $routeProvider.when("/", {
         controller: "homeController",
-        templateUrl:"templates/home.html"
+        templateUrl: "templates/home.html"
     });
 
     $routeProvider.otherwise({
         redirectTo: '/'
     });
     //if (window.history && window.history.pushState) {
-   
+
     //    $locationProvider.html5Mode({
-    //        enabled: true,
-    //        requireBase: false
-    //    });
+    //       enabled: true,
+    //     requireBase: false
+    //});
     //}
 }]);
 
-app.controller('homeController', ["$scope", "$http",  function ($scope, $http) {
- $('.parallax').parallax();
- 
+app.controller('homeController', ["$scope", "$http", function ($scope, $http) {
+    $('.parallax').parallax();
+
 }]);
